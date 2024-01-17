ea5f58cd138bb997d28647bad87cfd17d2763139
Mate720 <90619512+Mate720@users.noreply.github.com>
2024-01-17 09:11:04 +0100

    Random.html és random.js létrehozása
    Véletlenszám generátor

---
+++ random.js
@@ -0,0 +1,10 @@
+function random() {
+    document.getElementById("veletlen").innerHTML = Math.floor(Math.random() * 101); //Máté
+}
+
+function torol() {
+    veletlen.innerHTML ="0"; //Attila
+}
+
+
+
